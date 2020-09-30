4c2748f9f88f2e7f7e287577edcae4f889465a0e
Kery3330 <58009727+karina1998evseenkova@users.noreply.github.com>
2020-09-30 14:29:33 +0300

    Add files via upload
    This project's my first work with PHP

---
+++ templates/main.php
@@ -0,0 +1,13 @@
+ <div id="promo">
+     <h1 id="promoText">
+         Новый PhoneBrand - это лучший смартфон
+     </h1>
+ </div>
+
+ <div id="mainTextWrap">
+     <div id="mainText">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium placeat, deleniti voluptatum, blanditiis hic laboriosam quis dicta quisquam incidunt quaerat quo aperiam mollitia expedita rerum quibusdam! Sed fugiat quidem maiores?
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ut, repudiandae labore ex, aliquam obcaecati minima alias modi neque esse dolorem nisi sunt ipsa similique nostrum porro! Ullam, perferendis magnam!
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, quam ea, corporis natus unde repudiandae vitae tenetur suscipit officiis cum repellendus maxime, nemo sapiente ut illum voluptates ipsum nulla, magni.
+     </div>
+ </div>
